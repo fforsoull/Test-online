d2c500806b3a2f00d081d04f8e74e1c57e387885
fforsoull <69221370+fforsoull@users.noreply.github.com>
2026-09-02 14:24:00 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Конфигурация сборки. Ничего менять не нужно.
-export default defineConfig({
-  plugins: [react()],
-});
